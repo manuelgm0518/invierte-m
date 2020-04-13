ce9f5469f7a58735df942ee5e99a8b47eee59427
manuelgm0518 <manuel.gonzalez.0518@gmail.com>
2020-04-13 02:29:51 -0500

    Chat model fix

--- backend/models/Chat.js
+++ backend/models/Chat.js
@@ -3,17 +3,17 @@ const { Schema, model } = require('mongoose');
 var ChatSchema = Schema({
     user1: {
         user: {type: Schema.Types.ObjectId, ref: 'User', required: [true, 'Ingrese al usuario 1']},
-        messages: {
+        messages: [{
             sent: { type: Date, default: Date.now },
             content: String
-        }
+        }]
     },
     user2: {
         user: {type: Schema.Types.ObjectId, ref: 'User', required: [true, 'Ingrese al usuario 2']},
-        messages: {
+        messages: [{
             sent: { type: Date, default: Date.now },
             content: String
-        }
+        }]
     }
 });
 
