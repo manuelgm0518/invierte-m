78cd70c61b6f4b14f9ebe243b671bc4547c0305e
manuelgm0518 <manuel.gonzalez.0518@gmail.com>
2020-04-13 22:06:54 -0500

    Removed redundant cors

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt')
 
 const User = require('../models/User');
-router.use(cors());
 
 process.env.SECRET_KEY = 'secret';
 
@@ -15,70 +13,69 @@ router.post('/', (req, res) => {
     User.findOne({
         email: req.body.email
     })
-    .then(user => {
-        if(!user){
-            bcrypt.hash(req.body.password, 10, (err, hash) => {
-                userData.password = hash;
-                User.create(userData, (err, doc) => {
-                    if(err){
-                        res.status(400).json(err);
-                        return;
-                    } 
-                    res.json(doc);
+        .then(user => {
+            if (!user) {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    userData.password = hash;
+                    User.create(userData, (err, doc) => {
+                        if (err) {
+                            res.status(400).json(err);
+                            return;
+                        }
+                        res.json(doc);
+                    });
                 });
-            });
-            
-        }
-        else{
-            res.json({error: 'User already exists'});
-        }
-    });
+
+            }
+            else {
+                res.json({ error: 'User already exists' });
+            }
+        });
 });
 
 router.post('/logIn', (req, res) => {
     User.findOne({
-        email:req.body.email
+        email: req.body.email
     })
-    .then(user => {
-        if(user){
-            if(bcrypt.compareSync(req.body.password, user.password)){
-                const payload = {
-                    _id: user._id,
-                    firstName: user.firstName,
-                    lastName: user.lastName,
-                    emai: user.email
-                };
-                let token = jwt.sign(payload, process.env.SECRET_KEY, {
-                    expiresIn: 1440  
-                });
-                res.send(token);
+        .then(user => {
+            if (user) {
+                if (bcrypt.compareSync(req.body.password, user.password)) {
+                    const payload = {
+                        _id: user._id,
+                        firstName: user.firstName,
+                        lastName: user.lastName,
+                        emai: user.email
+                    };
+                    let token = jwt.sign(payload, process.env.SECRET_KEY, {
+                        expiresIn: 1440
+                    });
+                    res.send(token);
+                }
+                else {
+                    res.json({ error: 'User does not exist' });
+                }
             }
-            else{
-                res.json({error: 'User does not exist'});
+            else {
+                res.json({ error: 'User does not exist' });
             }
-        }
-        else{
-            res.json({error: 'User does not exist'});
-        }
-    })
-    .catch(err => {
-        //res.send('error: ' + err);
-        res.json({error: 'User does not exist'});
-    });
+        })
+        .catch(err => {
+            res.json({ error: 'User does not exist', log: err });
+        });
 });
 
 router.get('/', (req, res) => {
     let token = req.headers.token;
     jwt.verify(token, process.env.SECRET_KEY, (err, decode) => {
-        if(err){
-            res.status(401, json({
+        if (err) {
+            res.status(401).json({
                 error: 'unauthorized'
-            }));
+            });
             return;
         }
-        else{
+        else {
             res.json({
-                _id:decode._id,
+                _id: decode._id,
                 firstName: decode.firstName,
                 lastName: decode.lastName,
                 emai: decode.email
