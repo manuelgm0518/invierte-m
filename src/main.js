9a93b3792f10758d300e2e8562409b87688c7174
manuelgm0518 <manuel.gonzalez.0518@gmail.com>
2020-04-14 00:40:05 -0500

    Added Vuex Store

--- src/main.js
+++ src/main.js
@@ -9,21 +9,26 @@ Vue.config.productionTip = false;
 Vue.use(Vuex);
 Vue.use(BootstrapVue, {
   "BTooltip": {
-    "boundary": "scrollParent",
-    "boundaryPadding": 5,
-    "customClass": null,
-    "delay": 50,
     "variant": "golden"
   },
   "BDropdown": {
-    "toggleText": "Toggle Dropdown",
-    "size": null,
-    "variant": "golden  ",
-    "splitVariant": null
+    "variant": "golden"
   },
+});
+
+const store = new Vuex.Store({
+  state: {
+      userId: null,
+  },
+  mutations: {
+    userLogin(state, user) {
+      state.loggedUser = user;
+    }
+  }
 });
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
