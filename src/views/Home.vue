c5e6d96cfeea6223f70c665d1e5942d4667f34ab
manuelgm0518 <manuel.gonzalez.0518@gmail.com>
2020-04-16 01:08:18 -0500

    Fixed shopping cart

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,41 @@
 <template>
-	<b-container fluid class="p-0">
-		<div class="area">
+	<b-container fluid class="p-0 m-0 w-100">
+		<!--<div class="area">
 			<ul class="circles">
 				<li v-for="index in 20" :key="index"></li>
 			</ul>
-		</div>
-		<b-row class="bg-white text-center m-0 vh-100 landing">
-			<b-col class="text-center">
-				<div class="my-auto">
-					<b-img src="../assets/inviertem-logo-alt.svg" style="height: 50vh;"></b-img>
-					<p class="text-white h4 w-75 mx-auto mt-4">Una plataforma en la cual todos podemos ayudar a impulsar al engranaje de la economía Mexicana</p>
-					<i class="fas fa-chevron-down text-white"></i>
-				</div>
+		</div>-->
+
+		<b-row class="bg-white text-center w-100">
+			<b-col class="landing">
+				<b-img src="../assets/inviertem-logo-alt.svg" style="width: 10vh;"></b-img>
+				<p
+					class="text-white h4 w-75 mx-auto mt-4"
+				>Una plataforma en la cual todos podemos ayudar a impulsar al engranaje de la economía Mexicana</p>
+				<i class="fas fa-chevron-down text-white"></i>
 			</b-col>
 		</b-row>
 
-    <b-row class="bg-white p-3 w-100">
-      <b-col>
-        <h1>Sección no c k</h1>
-      </b-col>
-    </b-row>
-
-		<b-row class="bg-golden text-center m-0 p-4">
+		<b-row class="bg-white w-100 p-3">
 			<b-col>
+				<h1>Sección no c k</h1>
+			</b-col>
+		</b-row>
+
+		<b-row class="bg-golden text-center m-0 p-0">
+			<b-col cols="12" md="4">
 				<h3>
 					<b>Misión</b>
 				</h3>
 				<p>Nuestra misión es blablabla</p>
 			</b-col>
-			<b-col>
+			<b-col cols="12" md="4">
 				<h3>
 					<b>Visión</b>
 				</h3>
 				<p>Nuestro objetivo es gsomsemfksef</p>
 			</b-col>
-			<b-col>
+			<b-col cols="12" md="4">
 				<h4>
 					<b>Objetivo</b>
 				</h4>
@@ -53,8 +54,8 @@ export default {
 <style lang="scss" scoped>
 @import "../assets/anim-landing.scss";
 .landing {
-    display: flex;
-    align-items: center;
-    flex-wrap: wrap;
+	display: flex;
+	align-items: center;
+	flex-wrap: wrap;
 }
 </style>
--- src/views/ShoppingCart.vue
+++ src/views/ShoppingCart.vue
@@ -1,61 +1,64 @@
 <template>
-  <div>
-    <h1>Carrito de compra</h1>
-    <div class="shoppingCart">
-      <div v-for="(product, i) in shoppingCart" v-bind:key="i" class="productS">
-        <div class="productImageS">
-          <img v-bind:src="product.product.imageURL">
-        </div>
-        <div class="productNameS">
-          {{ product.product.name }}
-        </div>
-        <div class="productQuantityS">
-          {{ product.quantity }}
-        </div>
-        <button class="productRemoveS">X</button>
-      </div>
-    </div>
-  </div>
+	<div>
+		<h1>Carrito de compra {{ $store.state.user.id }}</h1>
+		<div class="shoppingCart">
+			<div v-for="(product, i) in shoppingCart" v-bind:key="i" class="productS">
+				<div class="productImageS">
+					<img v-bind:src="product.product.imageURL" />
+				</div>
+				<div class="productNameS">{{ product.product.name }}</div>
+				<div class="productQuantityS">{{ product.quantity }}</div>
+				<button class="productRemoveS">X</button>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  name: 'ShoppingCart',
-  data(){
-    return {
-      shoppingCart:{}
-    }
-  },
-  mounted(){
-    axios.get('http://localhost:3000/api/user/' + this.$store.state.user.id)
-    .then(res => {
-      this.shoppingCart = res.data.shoppingCart;
-    });
-  }
-}
+	name: "ShoppingCart",
+	data() {
+		return {
+			shoppingCart: {}
+		};
+	},
+	mounted() {
+		new Promise(() => {
+			setTimeout(() => {
+				axios
+					.get("http://localhost:3000/api/user/" + this.$store.state.user.id)
+					.then(res => {
+						this.shoppingCart = res.data.shoppingCart;
+					});
+			}, 50);
+		});
+	}
+};
 </script>
 
 <style>
-.shoppingCart{
-  margin: 20px;
+.shoppingCart {
+	margin: 20px;
 }
-.productS{
-  box-shadow: 0px 0px 10px -2px rgba(0,0,0,0.5);
-  padding: 5px;
-  margin: 10px 0;
-  border-radius: 5px;
+.productS {
+	box-shadow: 0px 0px 10px -2px rgba(0, 0, 0, 0.5);
+	padding: 5px;
+	margin: 10px 0;
+	border-radius: 5px;
 }
-.productImageS, .productNameS, .productQuantityS{
-  display: inline-block;
-  padding: 5px;
+.productImageS,
+.productNameS,
+.productQuantityS {
+	display: inline-block;
+	padding: 5px;
 }
-.productImageS img{
-  height: 40px;
+.productImageS img {
+	height: 40px;
 }
-.productRemoveS{
-  position: relative;
-  right: 0;
+.productRemoveS {
+	position: relative;
+	right: 0;
 }
 </style>
